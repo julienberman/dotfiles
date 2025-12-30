17def21613629d2f9040cb39def5a065237c4620
Julien Berman <julien.d.berman@gmail.com>
2025-12-30 00:12:19 -0500

    add commentary plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -930,10 +930,7 @@ require("lazy").setup({
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	-- { import = 'custom.plugins' },
-	--
+	{ import = "plugins" },
 	-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
 	-- Or use telescope!
 	-- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
--- nvim/.config/nvim/lua/plugins/init.lua
+++
@@ -1,2 +0,0 @@
--- Add plugins here
-return {}
