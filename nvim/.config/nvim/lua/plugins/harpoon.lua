221e0afa8d121864d8fdb332fbdf2a34a10ffe43
Julien Berman <julien.d.berman@gmail.com>
2026-01-04 23:08:05 -0500

    fix harpoonage

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,10 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
 	opts = {
 		menu = {
 			width = vim.api.nvim_win_get_width(0) - 4,
@@ -12,19 +16,18 @@ return {
 	keys = function()
 		local keys = {
 			{
-				"<leader>H",
+				"<leader>M",
 				function()
 					require("harpoon"):list():add()
 				end,
 				desc = "Harpoon File",
 			},
 			{
-				"<leader>h",
+				"<leader>m",
 				function()
-					local harpoon = require("harpoon")
-					harpoon.ui:toggle_quick_menu(harpoon:list())
+					require("telescope").extensions.harpoon.marks()
 				end,
-				desc = "Harpoon Quick Menu",
+				desc = "Harpoon Marks (Telescope)",
 			},
 		}
 
