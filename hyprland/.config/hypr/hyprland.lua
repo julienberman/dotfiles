277668a23bb76c66a5c2eb7e4ecdd0b4363e6859
Julien Berman <julien.d.berman@gmail.com>
2026-06-19 17:21:12 -0400

    Add uwsm config, fix env vars in hyprland config

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -5,10 +5,6 @@ hl.monitor({
 	scale = "auto",
 })
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("SSH_AUTH_SOCK", "$XDG_RUNTIME_DIR/ssh-agent.socket")
-
 require("autostart")
 require("keybindings")
 
