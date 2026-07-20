63b9d96ce5b97ceffd0ecab9007de6b4dca3e39b
Julien Berman <julien.d.berman@gmail.com>
2026-07-20 15:58:13 -0400

    remove format on save

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -26,10 +26,6 @@ return {
 				lua = { "stylua" },
 				python = { "ruff_format", "ruff_organize_imports", "ruff_format" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
 		},
 	},
 }
